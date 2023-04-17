eac46493979dacf85e843579522b3062aa02ba1d
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-04-16 19:14:03 -0700

    Adds first attempt at solution

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  for (const letter of word) {
+    const letterIndex = word.indexOf(letter)
+    const rightLetter = word[word.length-letterIndex-1]
+    if (letter !== rightLetter) {
+      return false
+    } else {
+      return true
+    }
+}}
+
 
 /* 
   Add your pseudocode here
+
+  - for each letter in the word, get the index of the letter
+  - get the letter the same distance away from the end of the string 
+  - if the two letters are not the same, false; else, true
+  
 */
 
 /*
